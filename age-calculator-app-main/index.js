d57c1d07f070abbc562360082eb32985a97ed634
polskipietra <109820324+polskipietra@users.noreply.github.com>
2023-08-17 21:33:26 +0200

    Add files via upload

---
+++ age-calculator-app-main/index.js
@@ -0,0 +1,16 @@
+// input date values
+const dayInput = document.querySelector('.day')
+const monthInput = document.querySelector('.month')
+const yearInput = document.querySelector('.year')
+const inputEssentials = document.querySelector('.input-essentials')
+const errorMessage = document.querySelector('.error-message')
+// button 
+const calcButton = document.querySelector('.calc-button')
+// display date values 
+const displayYears = document.querySelector('.calc-years')
+const displayMonths = document.querySelector('.calc-months')
+const displayDays = document.querySelector('.calc-days')
+// functions 
+const inputValidator = () => {
+    
+}
